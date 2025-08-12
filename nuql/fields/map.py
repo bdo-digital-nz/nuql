c3417e9d7014231cb1a5fcf4a789585b4a10b31e
lukeshortland <luke@shortland.io>
2025-08-12 13:48:32 +1200

    Refactor: Extract serialisation logic to `serialise_internal` and add `deserialise` method in `Map` field

--- nuql/fields/map.py
+++ nuql/fields/map.py
@@ -22,46 +22,16 @@ class Map(resources.FieldBase):
         self.fields = resources.create_field_map(self.config['fields'], self.parent, self.parent.provider.fields)
         self.serialiser = resources.Serialiser(self)
 
-    def __call__(self, value: Any, action: 'types.SerialisationType', validator: 'resources.Validator') -> Any:
-        """
-        Encapsulates the internal serialisation logic to prepare for
-        sending the record to DynamoDB.
-
-        :arg value: Deserialised value.
-        :arg action: SerialisationType (`create`, `update`, `write` or `query`).
-        :arg validator: Validator instance.
-        :return: Serialised value.
-        """
-        has_value = not isinstance(value, resources.EmptyValue)
-
-        # Apply generators if applicable to the field to overwrite the value
-        if action in ['create', 'update', 'write']:
-            if action == 'create' and self.on_create:
-                value = self.on_create()
-
-            if action == 'update' and self.on_update:
-                value = self.on_update()
-
-            if self.on_write:
-                value = self.on_write()
-
-        # Set default value if applicable
-        if not has_value and not value:
-            value = self.default
-
-        # Serialise the value
+    def serialise_internal(
+            self,
+            value: Any,
+            action: 'types.SerialisationType',
+            validator: 'resources.Validator'
+    ) -> Any:
+        """Serialises the Map value"""
         if value:
-            value = self.serialiser.serialise(action, value, validator)
+            return self.serialiser.serialise(action, value, validator)
 
-        # Validate required field
-        if self.required and action == 'create' and value is None:
-            validator.add(name=self.name, message='Field is required')
-
-        # Run internal validation
-        self.internal_validation(value, action, validator)
-
-        # Run custom validation logic
-        if self.validator and action in ['create', 'update', 'write']:
-            self.validator(value, validator)
-
-        return value
+    def deserialise(self, value: Any) -> Any:
+        """Deserialises the Map value"""
+        return self.serialiser.deserialise(value)
