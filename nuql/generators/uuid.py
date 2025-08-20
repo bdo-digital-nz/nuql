533b76ed98b1d742784bd559c6542383c945d5d2
lukeshortland <luke@shortland.io>
2025-08-20 16:32:39 +1200

    Refactor: Improve `Uuid` class by handling `uuid7` import errors and moving imports inside methods

--- nuql/generators/uuid.py
+++ nuql/generators/uuid.py
@@ -1,12 +1,12 @@
 __all__ = ['Uuid']
 
-from uuid_utils import uuid4, uuid7
-
 
 class Uuid:
     @classmethod
     def v4(cls):
         """Generates a random UUID v4"""
+        from uuid import uuid4
+
         def generator():
             return uuid4()
         return generator
@@ -14,6 +14,12 @@ class Uuid:
     @classmethod
     def v7(cls):
         """Generates a random UUID v7"""
+        try:
+            from uuid_utils import uuid7
+        except ImportError:
+            raise ImportError('Dependency "uuid_utils" must be installed to use UUID v7 generation.')
+
         def generator():
             return uuid7()
+
         return generator
